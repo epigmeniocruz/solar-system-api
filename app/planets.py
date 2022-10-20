f11fdda098b0627b2a4c641d404619f9994ffc3f
nina-tuyen tran <ninatuyentran@gmail.com>
2022-10-20 11:51:26 -0700

    defined Planet class

---
+++ app/planets.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, jsonify
+
+class Planet:
+    def __init__(self, id, name, description, size):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.size = size
+
+planets = [
+    Planet(1, "Auberon", "rocky", 10000),
+    Planet(2, "Jerica", "icy", 20000),
+    Planet(3, "Nancy", "hot", 5000),
+    Planet(4, "Gowen", "sandy", 30000),
+]
+
+planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planet")
+
+@planet_bp.route("", methods=["GET"])
+def get_all_planets():
+    response = []
+    for planet in planets:
+        planet_dict = {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "size": planet.size
+            }
+        response.append(planet_dict)
+    return jsonify(response), 200
