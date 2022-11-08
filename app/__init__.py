2236446cded6f146715fab0ad447591f20f7876d
Shweet Abraha <sabraha0311@gmail.com>
2022-11-08 11:36:19 -0800

    init file update from merge.

--- app/__init__.py
+++ app/__init__.py
@@ -15,14 +15,10 @@ def create_app(testing=None):
     if testing is None:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
-<<<<<<< HEAD
-    from app.Models.planets import Planet
-=======
     else:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
     
-    from app.models.planets import Planet
->>>>>>> f9096b15a0041b6a85297f2395e5ba37b8db28c1
+    from app.Models.planets import Planet
 
     db.init_app(app)
     migrate.init_app(app, db)
